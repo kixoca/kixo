c3d64f9a1f51d1008442a925ee9328cfcf794bbc
Jonathan Roy <jroy@optimumweb.ca>
2012-09-16 21:14:09 -0400

    Init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kixo::Application.config.secret_token = 'a5aaed713a10418577944b9188a1474378e14e0a9468134b86522eb7526526f2faa2abcfd585e7144cde2ed33dac9b71a57c1f61afc85230f0a2d81c06d41303'
