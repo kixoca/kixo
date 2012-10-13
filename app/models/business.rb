66ca8e71182fe6279e35ffb2c70bc20834920439
Jonathan Roy <jroy@optimumweb.ca>
2012-10-13 16:38:02 -0400

    Fix migrations

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,7 +1,7 @@
 class Business < ActiveRecord::Base
 
   # a business can have many locations (physical addresses)
-  has_many :locations
+  has_many :locations, :dependent => :destroy
 
   validates :slug, :presence => true, :uniqueness => true
   validates :name, :presence => true
