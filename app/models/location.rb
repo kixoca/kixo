b498300086e4a95953f8865e1429f53aee4caad2
Jonathan Roy <jroy@optimumweb.ca>
2012-10-14 17:43:11 -0400

    Fix model relationships

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,15 +7,13 @@ class Location < ActiveRecord::Base
   # every location is linked to a business
   belongs_to :business
 
+  has_one :locality,    :class_name => 'Where', :foreign_key => 'locality_id'
+  has_one :region,      :class_name => 'Where', :foreign_key => 'locality_id'
+  has_one :postal_code, :class_name => 'Where', :foreign_key => 'locality_id'
+  has_one :country,     :class_name => 'Where', :foreign_key => 'locality_id'
+
   # make sure the parent business exists
   validates_existence_of :business
-
-  # street, locality, region, postal code and country
-  # are stored in the Where model
-  has_one :locality,    :class_name => "Where", :foreign_key => "locality_id"
-  has_one :region,      :class_name => "Where", :foreign_key => "region_id"
-  has_one :postal_code, :class_name => "Where", :foreign_key => "postal_code_id"
-  has_one :country,     :class_name => "Where", :foreign_key => "country_id"
 
   validates :door_number,     :presence => true
   validates :house_number,    :presence => true
--- app/models/where.rb
+++ app/models/where.rb
@@ -2,6 +2,9 @@ class Where < ActiveRecord::Base
 
   attr_accessible :value, :level
 
+  # every Where is linked with a location
+  belongs_to :location
+
   validates :value, :presence => true
   validates :level, :presence => true
 
