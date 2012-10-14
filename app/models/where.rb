87900ca0375139c11b69ee51746e860dbd7f2379
Jonathan Roy <jroy@optimumweb.ca>
2012-10-14 17:59:49 -0400

    Fix model relationships

--- app/models/where.rb
+++ app/models/where.rb
@@ -2,9 +2,6 @@ class Where < ActiveRecord::Base
 
   attr_accessible :value, :level
 
-  # every Where is linked with a location
-  belongs_to :location
-
   validates :value, :presence => true
   validates :level, :presence => true
 
