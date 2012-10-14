2c07f8d58ab2288f9c86bcc887d3014f665df655
Jonathan Roy <jroy@optimumweb.ca>
2012-10-14 18:12:13 -0400

    Rethink models

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,26 +1,19 @@
 class Location < ActiveRecord::Base
 
-  attr_accessible :business_id, :door_number, :house_number, :street_name,
-                  :locality_id, :region_id, :postal_code_id, :country_id,
-                  :tel, :fax, :email
+  attr_accessible :business_id, :door_number, :house_number, :street_name, :locality,
+                  :region, :postal_code, :country, :tel, :fax, :email
 
   # every location is linked to a business
   belongs_to :business
 
-  has_one :locality,    :class_name => 'Where', :primary_key => :locality_id
-  has_one :region,      :class_name => 'Where', :primary_key => :region_id
-  has_one :postal_code, :class_name => 'Where', :primary_key => :postal_code_id
-  has_one :country,     :class_name => 'Where', :primary_key => :country_id
-
   # make sure the parent business exists
   validates_existence_of :business
 
-  validates :door_number,     :presence => true
-  validates :house_number,    :presence => true
-  validates :street_name,     :presence => true
-  validates :locality_id,     :presence => true
-  validates :region_id,       :presence => true
-  validates :postal_code_id,  :presence => true
-  validates :country_id,      :presence => true
+  validates :house_number, :presence => true
+  validates :street_name,  :presence => true
+  validates :locality,     :presence => true
+  validates :region,       :presence => true
+  validates :postal_code,  :presence => true
+  validates :country,      :presence => true
 
 end
--- db/migrate/20121010041526_create_locations.rb
+++ db/migrate/20121010041526_create_locations.rb
@@ -3,12 +3,12 @@ class CreateLocations < ActiveRecord::Migration
     create_table :locations do |t|
       t.integer :business_id
       t.string  :door_number
-      t.string  :house_number,     :null => false
-      t.string  :street_name,      :null => false
-      t.integer :locality_id,      :null => false
-      t.integer :region_id,        :null => false
-      t.integer :postal_code_id,   :null => false
-      t.integer :country_id,       :null => false
+      t.string  :house_number, :null => false
+      t.string  :street_name,  :null => false
+      t.string  :locality,     :null => false
+      t.string  :region,       :null => false
+      t.string  :postal_code,  :null => false
+      t.string  :country,      :null => false
       t.string  :tel
       t.string  :fax
       t.string  :email
