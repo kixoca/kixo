bef512abb81d4f7d62e163bd8b8175b264dda3a1
Jonathan Roy <jroy@optimumweb.ca>
2012-10-13 23:46:20 -0400

    Design business views

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20121010041526) do
     t.string   "email"
     t.string   "url"
     t.string   "logo"
+    t.text     "description"
     t.string   "facebook_url"
     t.string   "twitter_url"
     t.string   "linkedin_url"
