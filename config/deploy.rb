3454369c40da47f4b35209a8983711127cd9d63f
Jonathan Roy <jroy@optimumweb.ca>
2012-10-17 17:39:52 -0400

    New mod_rails deployment technique

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,10 +15,20 @@ server "kixo.ca", :app, :web, :db, :primary => true
 set :user, "kixo"
 
 # deployment with phusion passenger (mod_rails)
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
+
+after :deploy, "passenger:restart"
