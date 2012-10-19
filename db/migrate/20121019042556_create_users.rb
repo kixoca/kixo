c33aad34dd65c9a60ab294bddcb2302f95f933e1
Jonathan Roy <jroy@optimumweb.ca>
2012-10-19 00:29:12 -0400

    Add email and password cols

--- db/migrate/20121019042556_create_users.rb
+++ db/migrate/20121019042556_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
       t.timestamps
     end
   end
