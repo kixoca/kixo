5b90c809bc757e8e10ef551c633be0592c42b350
Jonathan Roy <jroy@optimumweb.ca>
2012-10-19 01:28:19 -0400

    Integrate users

--- db/migrate/20121019042556_create_users.rb
+++ db/migrate/20121019042556_create_users.rb
@@ -1,8 +1,24 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      # authenticable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
+
+      # recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      # rememberable
+      t.datetime :remember_created_at
+
+      # trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
       t.timestamps
     end
   end
