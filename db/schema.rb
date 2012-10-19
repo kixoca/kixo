b991bb9c81955bd4cc5c571a2d46b417e7561b5a
Jonathan Roy <jroy@optimumweb.ca>
2012-10-19 00:40:14 -0400

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121015043251) do
+ActiveRecord::Schema.define(:version => 20121019042556) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -102,6 +102,13 @@ ActiveRecord::Schema.define(:version => 20121015043251) do
     t.string   "email",      :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",              :default => "", :null => false
+    t.string   "encrypted_password", :default => "", :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
   end
 
 end
