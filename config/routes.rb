9909ea3d150a7f66eed6cb8fdab11f94ac9aa15c
Jonathan Roy <jroy@optimumweb.ca>
2012-10-19 00:30:47 -0400

    Add devise routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Kixo::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users
 
   resources :businesses do
     resources :locations
