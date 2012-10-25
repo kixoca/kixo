46226c1a044a6e87809f8e2dc6b8d9dfa92749cc
Jonathan Roy <jroy@optimumweb.ca>
2012-10-25 02:02:35 -0400

    Clean dashboard of vestiges

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,27 +3,6 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    columns do
-      column do
-        panel "Recent Businesses" do
-          ul do
-            Business.last(5).map do |business|
-              li link_to(business.name, admin_business_path(business))
-            end
-          end
-        end
-      end
-
-      column do
-        panel "Recent Locations" do
-          ul do
-            Location.last(10).map do |location|
-              li link_to(location.business.name + " - " + location.locality, admin_location_path(location))
-            end
-          end
-        end
-      end
-    end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
