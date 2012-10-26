f91a5deeda7f3acc8e32ef2486d8f0e2b54e6bf1
Jonathan Roy <jroy@optimumweb.ca>
2012-10-25 23:32:14 -0400

    Only one model can be omniauthable, and it will be users

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,8 +1,7 @@
 class Professional < ActiveRecord::Base
 
   devise :database_authenticatable,
-         :registerable, :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable
+         :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid,
                   :name, :tel, :street_address_1, :street_address_2, :locality, :region, :postal_code, :country,
