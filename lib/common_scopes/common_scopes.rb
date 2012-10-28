da1234ef3f0430cc84609c46a9e19c9aad2acef5
Jonathan Roy <jroy@optimumweb.ca>
2012-10-28 02:47:12 -0400

    Add a CommonScopes module to lib

---
+++ lib/common_scopes/common_scopes.rb
@@ -0,0 +1,2 @@
+module CommonScopes
+end
---
+++ lib/common_scopes/locale.rb
@@ -0,0 +1,7 @@
+module CommonScopes::Locales
+  def self.included(base)
+    base.class_eval do
+      named_scope :by_locale, lambda {|*locale| {:conditions => {:locale => locale.first || I18n.locale}}}
+    end
+  end
+end
