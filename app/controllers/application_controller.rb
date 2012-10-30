1e895c16a8a1ee2a9bd0cf6f42eca09bd21d399e
Jonathan Roy <jroy@optimumweb.ca>
2012-10-30 17:12:00 -0400

    Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,10 @@
 class ApplicationController < ActionController::Base
 
   # I18n
-
   before_filter :set_locale
 
   def set_locale
-    I18n.locale = extract_locale_from_subdomain || I18n.default_locale
-  end
-
-  def extract_locale_from_subdomain
-    parsed_locale = request.subdomains.first
-    parsed_locale && I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
 end
