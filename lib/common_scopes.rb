6b90d4cd2c8b666cda8995db594321623cd17ff4
Jonathan Roy <jroy@optimumweb.ca>
2012-10-30 17:28:22 -0400

    Simplify lib

--- lib/common_scopes.rb
+++ lib/common_scopes.rb
@@ -1,4 +1,4 @@
-module CommonScopes::Locales
+module CommonScopes
   def self.included(base)
     base.class_eval do
       named_scope :by_locale, lambda {|*locale| {:conditions => {:locale => locale.first || I18n.locale}}}
