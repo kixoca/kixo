75b1f240b1c39e8c912c7658ba07ae5d9ca436b4
Jonathan Roy <jroy@optimumweb.ca>
2012-10-31 22:43:47 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Kixo::Application.routes.draw do
 
   ActiveAdmin.routes(self)
 
-  devise_for :users
-  devise_for :professionals
-  devise_for :representants
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   # I18n
   scope "(:locale)", :locale => /en|fr/ do
+
+    devise_for :users
+    devise_for :professionals
+    devise_for :representants
 
     resources :professionals
     resources :questions
