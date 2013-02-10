7d74c8883acb954ad6d14d93530dd245f2e7ff51
Jonathan Roy <jroy@optimumweb.ca>
2013-02-10 00:14:54 -0500

    No need for a redundant model

--- app/models/subscription.rb
+++
@@ -1,9 +0,0 @@
-class Subscription < ActiveRecord::Base
-  attr_accessible :user, :user_id, :plan, :plan_id
-
-  belongs_to :plan
-  belongs_to :user
-
-  validates_existence_of :plan
-  validates_existence_of :user
-end
--- db/migrate/20130209232704_add_stripe_id_to_plans.rb
+++
@@ -1,5 +0,0 @@
-class AddStripeIdToPlans < ActiveRecord::Migration
-  def change
-    add_column :plans, :stripe_id, :string, :null => false, :default => ""
-  end
-end
