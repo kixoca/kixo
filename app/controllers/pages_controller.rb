f2a66d148994457861b26992649572e29030a209
Jonathan Roy <jroy@optimumweb.ca>
2013-06-05 16:47:37 -0400

    Pusher

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -47,6 +47,7 @@ class PagesController < ApplicationController
     @page = Page.find_by_name(params[:id])
 
     @page_title = t("pages.show.page_title", :title => @page.meta_title || @page.title)
+
   end
 
 end
