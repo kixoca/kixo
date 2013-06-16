c6258632b6c9470095ffccdf1d80caca8926a4b5
Jonathan Roy <jroy@optimumweb.ca>
2013-06-16 17:12:42 -0400

    Fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,11 @@ class ApplicationController < ActionController::Base
       elsif session[:locale]
         redirect_to root_url(:locale => session[:locale])
       elsif cookies[:locale]
-        redirect_to root_url(:locale => cookies[:locale])
+        redirect_to root_for(:locale => cookies[:locale])
       else
         browser_locale = request.env['HTTP_ACCEPT_LANGUAGE'].try(:scan, /^[a-z]{2}/).try(:first).try(:to_sym)
         if Locale.find_by_code(browser_locale)
-          redirect_to root_ul(:locale => browser_locale)
+          redirect_to root_url(:locale => browser_locale)
         else
           redirect_to root_url(:locale => I18n.default_locale)
         end
