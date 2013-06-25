4da1f85aa643f101fbcbf4f50166188bc8391290
Jonathan Roy <jroy@optimumweb.ca>
2013-06-25 12:42:41 -0400

    Admin for users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,32 @@ class Admin::UsersController < ApplicationController
   before_filter :authenticate_admin
 
   def index
-    @users = User.all
+    @users = User.all.order("created_at DESC")
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html do
+          flash[:success] = t("admin.user.update.success")
+          redirect_to edit_admin_user_path(@user)
+        end
+        format.json { head :no_content }
+        format.xml  { head :no_content }
+      else
+        format.html do
+          flash[:error] = t("admin.user.update.error")
+          render action: "edit"
+        end
+        format.json { render :json => @user.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 end
