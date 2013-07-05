ebbd712b7ce780ecf30e09188fcf44fa1824964d
Jonathan Roy <jroy@optimumweb.ca>
2013-07-04 23:32:57 -0400

    Minor dev fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -99,7 +99,7 @@ class User < ActiveRecord::Base
 
   # use paperclip to attach an headshot
   has_attached_file :headshot,
-                    :styles      => {:croppable => "500x500>", :large => "200x200#", :medium => "140x140#", :small => "100x100#", :thumb => "80x80#", :mini => "60x60#"},
+                    :styles      => {:large => "200x200#", :medium => "140x140#", :small => "100x100#", :thumb => "80x80#", :mini => "60x60#"},
                     :path        => "/headshots/:id/:style.:extension",
                     :default_url => "/headshots/defaults/:style.png"
 
