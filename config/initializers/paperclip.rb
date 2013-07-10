5c87e557de22b92acfe58523dc8bc7376c171c52
Jonathan Roy <jroy@optimumweb.ca>
2013-07-09 20:12:24 -0400

    Troubleshoot

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,8 +2,8 @@ Paperclip.options[:command_path] = "/usr/local/bin/"
 
 Paperclip::Attachment.default_options.merge!(
     :storage => :s3,
-    :bucket => ENV['AWS_BUCKET'],
     :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
