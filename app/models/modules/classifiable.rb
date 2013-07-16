666151f5f31d2eb34a48a7004c614c4e9129b20d
Jonathan Roy <jroy@optimumweb.ca>
2013-07-16 14:07:15 -0400

    Fix

--- app/models/modules/classifiable.rb
+++ app/models/modules/classifiable.rb
@@ -3,15 +3,15 @@ module Classifiable
     base.has_many :classifications, :as => :classifiable, :foreign_key => :classifiable_id
 
     def base.find_all_by_category(category)
-      self.joins(:categories).where(:conditions => {:professions => {:id => category}}).uniq
+      self.joins(:categories).where(:conditions => {:taxonomies => {:id => category}}).uniq
     end
 
     def base.find_all_by_topic(topic)
-      self.joins(:topics).where(:conditions => {:professions => {:id => topic}}).uniq
+      self.joins(:topics).where(:conditions => {:taxonomies => {:id => topic}}).uniq
     end
 
     def base.find_all_by_profession(profession)
-      self.joins(:professions).where(:conditions => {:professions => {:id => profession}}).uniq
+      self.joins(:professions).where(:conditions => {:taxonomies => {:id => profession}}).uniq
     end
 
     def base.search_by_category(term, locale = Locale.find_by_code(I18n.locale))
