c53f2ff47431bed67e0d16ac42dc9612619dfd4a
Jonathan Roy <jroy@optimumweb.ca>
2013-07-16 18:34:23 -0400

    Move fonts

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Kixo
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
     # Do not bootstrap application during asset compilation
     # config.assets.initialize_on_precompile = false
 
