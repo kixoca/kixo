2a88330590f20a2768e9f91104ce6078cd97290e
Jonathan Roy <jroy@optimumweb.ca>
2013-07-16 10:50:59 -0400

    Only in production

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,4 +1,4 @@
-if ENV['RACK_ENV'] == 'test' || ENV['RACK_ENV'] == 'production'
+if ENV['RACK_ENV'] == 'production'
   Kixo::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
     r301 /.*/,  Proc.new {|path, rack_env| "http://www.#{rack_env['SERVER_NAME']}#{path}" },     :if => Proc.new {|rack_env| ! (rack_env['SERVER_NAME'] =~ /www\./i)}
   end
